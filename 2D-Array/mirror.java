23ae14ef698bf3982588d5fe112edf13a7cf82dc
Anmol Anand <anmolanand2k15@gmail.com>
2022-02-20 20:16:35 +0530

    Final Commit

--- 2D-Array/mirror.java
+++ 2D-Array/mirror.java
@@ -24,25 +24,25 @@ class mirror {
       int m = sc.nextInt();
       System.out.println("Enter The Number Of Columns");
       int n = sc.nextInt();
-      int a[][]=new int[m][n];
-      int b[][]=new int[m][n];
+      int a[][]=new int[n][m];
+      int b[][]=new int[n][m];
       System.out.println("Enter The Elements Of Array");
-      for (int i=0;i<m;i++) {
-        for (int j=0;j<n;j++) {
+      for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
           a[i][j]=sc.nextInt();
         }
       }
       int k=0;
-      for (int i=0;i<m;i++) {
-        k = n-1;
-        for (int j=0;j<n;j++) {
+      for (int i=0;i<n;i++) {
+        k = m-1;
+        for (int j=0;j<m;j++) {
           b[i][j]=a[i][k];
           k--;
         }
       }
       System.out.println("Array After Mirroring The Elements");
-      for (int i=0;i<m;i++) {
-        for (int j=0;j<n;j++) {
+      for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
           System.out.print(b[i][j]+"\t");
         }
         System.out.println();
--- 2D-Array/swipe.java
+++ 2D-Array/swipe.java
@@ -24,25 +24,25 @@ class swipe {
       int m = sc.nextInt();
       System.out.println("Enter The Number Of Columns");
       int n = sc.nextInt();
-      int a[][]=new int[m][n];
-      int b[][]=new int[m][n];
+      int a[][]=new int[n][m];
+      int b[][]=new int[n][m];
       System.out.println("Enter The Elements Of Array");
-      for (int i=0;i<m;i++) {
-        for (int j=0;j<n;j++) {
+      for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
           a[i][j]=sc.nextInt();
         }
       }
       int k=0;
-      for (int j=0;j<n;j++) {
-        k = m-1;
-        for (int i=0;i<m;i++) {
+      for (int j=0;j<m;j++) {
+        k = n-1;
+        for (int i=0;i<n;i++) {
           b[i][j]=a[k][j];
           k--;
         }
       }
       System.out.println("Array After Swiping The Elements");
-      for (int i=0;i<m;i++) {
-        for (int j=0;j<n;j++) {
+      for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
           System.out.print(b[i][j]+"\t");
         }
         System.out.println();
