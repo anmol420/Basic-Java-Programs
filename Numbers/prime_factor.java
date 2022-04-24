481a4abbd2b33b42d1d6b02a23168533749d2602
Anmol Anand <anmolanand2k15@gmail.com>
2022-04-24 21:37:21 +0530

    Added Some Number Based Programs

---
+++ Numbers/prime_factor.java
@@ -0,0 +1,26 @@
+//Prime Factorisation
+
+import java.util.*;
+public class prime_factor {
+    static void prime_fac(int n) {
+        int m = 1,n1 = n;
+        System.out.print("Prime Factors Of "+n+"\n"+n+" - ");
+        for (int i=2;i<=n;i++) {
+            if (m == n) {
+                break;
+            } else {
+                while (n1%i==0) {
+                    m*=i;
+                    n1/=i;
+                    System.out.print(i+" ");
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A Number To Be Checked");
+        int n = sc.nextInt();
+        prime_fac(n);
+    }
+}
---
+++ Numbers/smith.java
@@ -0,0 +1,38 @@
+//Smith Number
+
+import java.util.*;
+public class smith {
+    static int sum_digit(int n) {
+        int sum = 0;
+        while (n>0) {
+            int d = n%10;
+            sum+= d;
+            n/=10;
+        }
+        return sum;
+    }
+    static int prime_fac(int n) {
+        int i=2,sum=0;
+        while (n>1) {
+            if (n%i==0) {
+                n/=i;
+                sum+= sum_digit(i);
+            } else {
+                i++;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number To Be Checked");
+        int n = sc.nextInt();
+        int k = sum_digit(n);
+        int p = prime_fac(n);
+        if (k==p) {
+            System.out.println(n+" Is A Smith Number");
+        } else {
+            System.out.println(n+" Is Not A Smith Number");
+        }
+    }
+}
