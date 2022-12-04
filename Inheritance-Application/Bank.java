aca5db2ca7c2600fbef692e7ab831b16377d9860
Anmol Anand <anmolanand2k15@gmail.com>
2022-12-04 22:55:24 +0530

    Added Changes

--- Inheritance-Application/Bank.java
+++ Inheritance-Application/Bank.java
@@ -1,46 +1,30 @@
 import java.util.*;
 class Bank {
-    String name;
-    long acc_no;
-    double bal;
-    Bank() {
-        name = "";
-        acc_no = 0;
-        bal = 0.0;
+    protected String name;
+    protected long acc_no;
+    protected double bal;
+    public Bank(String n,long a,double b) {
+        name = n;
+        acc_no = a;
+        bal = b;
     }
-    void accept() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the name");
-        name = sc.nextLine();
-        System.out.println("Enter the account number");
-        acc_no = sc.nextInt();
-        System.out.println("Enter the balance");
-        bal = sc.nextDouble();
-    }
-    void display() {
+    public void display() {
         System.out.println("Name: "+name);
         System.out.println("Account number: "+acc_no);
         System.out.println("Balance: "+bal);
     }
 }
 class Interest extends Bank {
-    double rate, time;
-    Interest() {
-        rate = 0.0;
-        time = 0.0;
+    private double rate, time;
+    public Interest(String n,long a,double b,double r,double t) {
+        super(n,a,b);
+        rate = r;
+        time = t;
     }
-    void accept() {
-        super.accept();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rate of interest");
-        rate = sc.nextDouble();
-        System.out.println("Enter the time");
-        time = sc.nextDouble();
-    }
-    double calculate() {
+    public double calculate() {
         return bal * Math.pow((1 + rate / 100), time) - bal;
     }
-    void display() {
+    public void display() {
         super.display();
         System.out.println("Rate of interest: "+rate);
         System.out.println("Time: "+time);
