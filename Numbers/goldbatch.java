9d6de8fe73d23b6ec6a0aba63b8d806b2bf57187
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-02-05 15:55:17 +0000

    Added Comment Lines

--- Numbers/goldbatch.java
+++ Numbers/goldbatch.java
@@ -4,14 +4,14 @@ package Numbers;
 
 import java.util.*;
 class goldbatch {
-    static boolean even(int n) {
+    static boolean even(int n) { // To Check If The Number Is Even
         if (n%2==0) {
             return true;
         } else {
             return false;
         }
-    }
-    static boolean odd_prime(int n) {
+    } // End Of even Method
+    static boolean odd_prime(int n) { // To Check If The Number Is Odd Prime
         int k=0,c=0;
         if (n%2!=0) {
             k++;
@@ -26,28 +26,29 @@ class goldbatch {
         } else {
             return false;
         }
-    }
-    public static void main(String[] args) {
+    } // End Of odd_prime Method
+    public static void main(String[] args) { // Main Method
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter A Number To Be Checked");
-        int n = sc.nextInt();
-        if (!even(n)) {
+        int n = sc.nextInt(); // Input The Number
+        if (!even(n)) { // If The Number Is Not Even
             System.out.println("Not A Even Number");
             return;
-        }
-        if (n<9 || n>50) {
+        } // End Of If
+        if (n<9 || n>50) { // If The Number Is Not In The Range
             System.out.println("Invalid");
             return;
-        }
+        } // End Of If
         System.out.println("Prime Pairs ->");
-        for (int i=n;i>=1;i--) {
-            for (int j=1;j<=i;j++) {
-                if ((i+j)==n) {
+        for (int i=1;i<=n;i++) { // Loop To Check The Prime Pairs
+            for (int j=1;j<=i;j++) { // Loop To Check The Prime Pairs
+                if ((i+j)==n) { // If The Sum Of The Two Numbers Is Equal To The Number
                     if (odd_prime(i) && odd_prime(j)) {
                         System.out.println(j+","+i);
-                    }
-                }
-            }
-        }
-    }
-}
+                    } // End Of If
+                } // End Of If
+            } // End Of Inner Loop
+        } // End Of Outer Loop
+        System.out.println("GoldBatch Number - "+n);
+    } // End Of Main Method
+}  // End Of Class
