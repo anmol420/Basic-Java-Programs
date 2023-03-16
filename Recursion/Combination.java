f8b108146feb6bcaa3a9337df8b2f13be9cccae5
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-03-16 17:27:36 +0000

    Added Program

---
+++ Recursion/Combination.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Combination {
+    int n,k;
+    public Combination() {
+        n=0;
+        k=0;
+    }
+    public static void read() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        n = in.nextInt();
+        System.out.print("Enter k: ");
+        k = in.nextInt();
+    }
+    public static int fact(int n) {
+        if(n==0)
+            return 1;
+        else
+            return n*fact(n-1);
+    }
+    public static void compute() {
+        int nfact = fact(n);
+        int kfact = fact(k);
+        int nkfact = fact(n-k);
+        int nck = nfact/(kfact*nkfact);
+        System.out.println("nCk = "+nck);
+    }
+    public static void display() {
+        System.out.println("n = "+n);
+        System.out.println("k = "+k);
+  
+    }
+    public static void main(String[] args) {
+        read();
+        display();
+        compute();
+    }
+}
